a8d144fee3d9c62dc1d9d60e7f1ce9db9c45f3b5
Rodrigo Soares <>
2022-09-04 04:03:06 -0300

    Add botões das letras no jogo e inicia a lista de palavras js

---
+++ game.js
@@ -0,0 +1,11 @@
+const listaFrutas = [
+    {palavra: "maçã", dica: "É vermelha e faz croc quando morde"},
+    {palavra: "banana", dica: "É amarela e macia"},
+]
+
+const listaProficoes = [
+    {palavra: "médico", dica: "Ajudam aqueles que mais precisam"},
+    {palavra: "bombeiro", dica: "Estão em calendários"},
+]
+
+const listaPalavras = [listaFrutas, listaProficoes];

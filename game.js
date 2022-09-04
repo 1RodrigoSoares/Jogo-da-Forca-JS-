b75088e3d0d8ed62a4e9392553c0d10b20a216a2
Rodrigo Soares <>
2022-09-04 20:32:28 -0300

    Add pagina de escolher tema + atualizaSelect() e localizaTema()

--- game.js
+++ game.js
@@ -7,5 +7,18 @@ const listaProficoes = [
     {palavra: "médico", dica: "Ajudam aqueles que mais precisam"},
     {palavra: "bombeiro", dica: "Estão em calendários"},
 ]
-
 const listaPalavras = [listaFrutas, listaProficoes];
+
+
+//TODA VEZ QUE MUDA A CAIXINHA DE SELEÇÃO DE TEMAS ELE ATUALIZA PARA PEGAR O VALOR ESCOLHIDO
+function atualizaSelect(){
+    const listaTemas = document.querySelector("#temas");
+    const opcaoSelecionada =  listaTemas.options[listaTemas.selectedIndex];
+    const valor = opcaoSelecionada.value;
+    localStorage.setItem("temaSelecionado", valor); //Salva o tema escolhido na memoria;
+}
+
+function localizaTemaSelecionado(){
+    const temaEscolhido = localStorage.getItem("temaSelecionado");
+    return temaEscolhido;
+}

c8c8315595c4381e66c484ed6c764c67979c6fad
Rodrigo Soares <>
2022-09-05 21:14:51 -0300

    ...

--- game.js
+++ game.js
@@ -302,25 +302,25 @@ function ajustaBoneco(){
     const espacoForca = document.querySelector("#imgBonecoJogo");
     if(letrasErradas == 1)
     {
-        espacoForca.src = "/img/hangman(cabeça).png";
+        espacoForca.src = "/imagem/hangman(cabeça).png";
     }else if(letrasErradas == 2)
     {
-        espacoForca.src = "/img/hangman(cabeça+corpo).png";
+        espacoForca.src = "/imagem/hangman(cabeça+corpo).png";
     }else if(letrasErradas == 3)
     {
-        espacoForca.src = "/img/hangman(cabeça+corpo+1braco).png";
+        espacoForca.src = "/imagem/hangman(cabeça+corpo+1braco).png";
     }else if(letrasErradas == 4)
     {
-        espacoForca.src = "/img/hangman(cabeça+corpo+doisbracos).png";
+        espacoForca.src = "/imagem/hangman(cabeça+corpo+doisbracos).png";
     }else if(letrasErradas == 5)
     {
-        espacoForca.src = "/img/hangman(quasecompleto).png";
+        espacoForca.src = "/imagem/hangman(quasecompleto).png";
     }else if(letrasErradas == 6)
     {
-        espacoForca.src = "/img/hangman(completo).png";
+        espacoForca.src = "/imagem/hangman(completo).png";
     }else
     {
-        espacoForca.src = "/img/hangman(morto).png";
+        espacoForca.src = "/imagem/hangman(morto).png";
     }
 }
 
